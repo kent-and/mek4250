b4f1cb9b851bbcb36a7aba60b542f17077c04160
Kent-Andre Mardal <kent-and@simula.no>
2021-03-24 15:16:38 +0100

    update

--- conv-diff-max.py
+++ conv-diff-max.py
@@ -8,7 +8,7 @@ prev_dLinf_error = 0
 for N in [8,16,32,64]:
 
   mesh = UnitSquareMesh(N, N)
-  V = FunctionSpace(mesh, "CG", 2)
+  V = FunctionSpace(mesh, "CG", 1)
   V3 = FunctionSpace(mesh, "CG", 4)
 
   u = TrialFunction(V)
@@ -37,9 +37,9 @@ for N in [8,16,32,64]:
 
   difference = project(u_analytical - U, V3) 
   Linf_error = max(difference.vector().max(), -difference.vector().min())
-  print (" N = %d, Linf error = %e " % (N, Linf_error))
+  print ((" N = %d, Linf error = %e " % (N, Linf_error)))
   if prev_Linf_error > 0: 
-    print "rate of Linf error ", math.log(prev_Linf_error / Linf_error, 2) 
+    print ("rate of Linf error ", math.log(prev_Linf_error / Linf_error, 2)) 
   prev_Linf_error = Linf_error 
 
   
@@ -48,7 +48,7 @@ for N in [8,16,32,64]:
   dLinf_error = max(difference.vector().max(), -difference.vector().min())
   print (" N = %d, dLinf error = %e " % (N, dLinf_error))
   if prev_dLinf_error > 0: 
-    print "rate of dLinf error ", math.log(prev_dLinf_error / dLinf_error, 2) 
+    print ("rate of dLinf error ", math.log(prev_dLinf_error / dLinf_error, 2))
   prev_dLinf_error = dLinf_error 
 
 
--- conv-diff.py
+++ conv-diff.py
@@ -14,16 +14,16 @@ for N in [8,16,32,64]:
   u = TrialFunction(V)
   v = TestFunction(V)
 
-  alpha_value = 1.0e-2 
-  alpha_value = 1.0 
-  alpha = Constant(alpha_value)
+  mu_value = 1.0e-2 
+  mu_value = 1.0 
+  mu = Constant(mu_value)
   f = Constant(0)
   h = mesh.hmin()
 
-  a = (-u.dx(0)*v + alpha*inner(grad(u),grad(v)))*dx  
+  a = (-u.dx(0)*v + mu*inner(grad(u),grad(v)))*dx  
   L = f*v*dx  
 
-  u_analytical = Expression("(exp(-x[0]/%e) - 1)/ (exp(-1/%e) - 1)" % (alpha_value, alpha_value), degree=3 )  
+  u_analytical = Expression("(exp(-x[0]/%e) - 1)/ (exp(-1/%e) - 1)" % (mu_value, mu_value), degree=3 )  
   def boundary(x, on_boundary):
       return on_boundary and (x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS)
 
@@ -34,16 +34,16 @@ for N in [8,16,32,64]:
 
   U_analytical = project(u_analytical, V3)
 
-  L2_error = errornorm(U, u_analytical) 
+  L2_error = errornorm(U, u_analytical, mesh = V.mesh()) 
   print (" N = %d, L2 error = %e " % (N, L2_error))
   if prev_L2_error > 0: 
-    print "rate of L2 error ", math.log(prev_L2_error / L2_error, 2) 
+    print ("rate of L2 error ", math.log(prev_L2_error / L2_error, 2)) 
   prev_L2_error = L2_error 
 
-  H1_error = errornorm(U, u_analytical, "H1") 
+  H1_error = errornorm(U, u_analytical, "H1", mesh = V.mesh()) 
   print (" N = %d, H1 error = %e " %  (N, H1_error))
   if prev_H1_error > 0: 
-    print "rate of H1 error ", math.log(prev_H1_error / H1_error, 2) 
+    print ("rate of H1 error ", math.log(prev_H1_error / H1_error, 2)) 
   prev_H1_error = H1_error 
 
 
